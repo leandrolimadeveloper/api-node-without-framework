37ff6baad4a07ab0afab0da8e9f24ff395b6f4ac
leandrolimadeveloper <leandroslimasr@gmail.com>
2023-06-30 11:38:33 -0300

    Create HTTP methods, file for the routes, and filtering to select data

--- src/database/database.js
+++ src/database/database.js
@@ -3,6 +3,8 @@ import fs from "node:fs/promises";
 const databasePath = new URL("../../db.json", import.meta.url);
 
 export class Database {
+    #database = {};
+
     constructor() {
         fs.readFile(databasePath, "utf-8")
             .then((data) => {
@@ -13,14 +15,24 @@ export class Database {
             });
     }
 
-    #database = {};
-
     #persist() {
         fs.writeFile(databasePath, JSON.stringify(this.#database));
     }
 
-    select(table) {
-        const data = this.#database[table] ?? [];
+    select(table, search) {
+        const data = this.#database !== null ? this.#database : null;
+
+        // if (search) {
+        //     data = data.filter((row) => {
+        //         return Object.entries(
+        //             search.some(([key, value]) => {
+        //                 return row[key]
+        //                     .toLowerCase()
+        //                     .includes(value.toLowerCase());
+        //             })
+        //         );
+        //     });
+        // }
 
         return data;
     }
@@ -35,5 +47,27 @@ export class Database {
         this.#persist();
 
         return data;
+    }
+
+    delete(table, id) {
+        const rowIndex = this.#database[table].findIndex(
+            (row) => row.id === id
+        );
+
+        if (rowIndex > -1) {
+            this.#database[table].splice(rowIndex, 1);
+            this.#persist();
+        }
+    }
+
+    update(table, id, data) {
+        const rowIndex = this.#database[table].findIndex(
+            (row) => row.id === id
+        );
+
+        if (rowIndex > -1) {
+            this.#database[table][rowIndex] = { id, ...data };
+            this.#persist();
+        }
     }
 }
